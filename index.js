c3b18fb4af827729e46999c0187c1cffc9404eda
STAFF\zhangwei36 <1004695936@qq.com>
2017-10-12 11:01:24 +0800

    static中间件，另外如果是修改了index.js文件，node服务要重启才能生效

--- index.js
+++ index.js
@@ -10,8 +10,10 @@ var handlebars = require('express3-handlebars')
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
-
 app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', function(req, res) {
     res.render('home'); // 用模板渲染home.handlebars并套入main.handlebars
